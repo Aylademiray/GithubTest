b3a31c84a67e6a4676565bf98c0721355a0ef37a
aylademiray <aylademiray1@gmail.com>
2019-11-17 22:55:10 -0500

    Added git ignore

--- src/Pet.java
+++ src/Pet.java
@@ -1,2 +1,20 @@
 public class Pet {
-}
+    private static String name;
+    private static Object color;
+    private static int age;
+    private static String breed;
+    private static String poisonus;
+
+
+    public static <name, color, age> void main(String[] args) {
+
+
+  Pet.name = "Snowball";
+  Pet.color="White";
+  Pet.age=2;
+  Pet.breed= "domestic";
+  Pet.poisonus="not";
+
+
+
+}}
---
+++ src/Phone.java
@@ -0,0 +1,15 @@
+public class Phone {
+     public String brand;
+     public  int memory;
+     public int gb;
+     public String color;
+    public String camera;
+     public int price;
+
+    public static void main(String[] args) {
+
+
+
+        }
+    }
+
