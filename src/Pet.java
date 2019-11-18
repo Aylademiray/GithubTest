1f02239cfb8bc3ec8b53138a0d48d326b6a9562d
aylademiray <aylademiray1@gmail.com>
2019-11-18 09:57:06 -0500

    added pet

---
+++ src/Pet.java
@@ -0,0 +1,12 @@
+public class Pet {
+public String name;
+public int age;
+public String color;
+public int weight;
+public int legs;
+
+    public void canJump(){}
+    public void isYoung() {}
+    public void isCute(){}
+}
+
